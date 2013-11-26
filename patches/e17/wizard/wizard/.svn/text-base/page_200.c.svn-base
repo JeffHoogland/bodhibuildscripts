13d7232e15d6efe296b4b8ced2adf024b0e77802
Jeff Hoogland <jeffhoogland@linux.com>
2013-11-25 22:10:41 -0600

    Updated E17/E18 scripts to build off of efl 1.8.0

---
+++ patches/e17/wizard/wizard/.svn/text-base/page_200.c.svn-base
@@ -0,0 +1,40 @@
+/* Delete previous copy of config profile and save new one */
+#include "e_wizard.h"
+
+#if 0
+EAPI int
+wizard_page_init(E_Wizard_Page *pg __UNUSED__, Eina_Bool *need_xdg_desktops __UNUSED__, Eina_Bool *need_xdg_icons __UNUSED__)
+{
+   return 1;
+}
+
+EAPI int
+wizard_page_shutdown(E_Wizard_Page *pg __UNUSED__)
+{
+   return 1;
+}
+
+EAPI int
+wizard_page_show(E_Wizard_Page *pg __UNUSED__)
+{
+   return 0; /* 1 == show ui, and wait for user, 0 == just continue */
+}
+
+EAPI int
+wizard_page_hide(E_Wizard_Page *pg __UNUSED__)
+{
+   return 1;
+}
+#endif
+EAPI int
+wizard_page_apply(E_Wizard_Page *pg __UNUSED__)
+{
+   // save the config now everyone has modified it
+   e_config_save();
+   // diusable restart env so we actually start a whole new session properly
+   e_util_env_set("E_RESTART", NULL);
+   // restart e
+   e_sys_action_do(E_SYS_RESTART, NULL);
+   return 1;
+}
+
---
+++ patches/e17/wizard/wizard/page_060.c
@@ -0,0 +1,62 @@
+#include "e.h"
+#include "e_mod_main.h"
+
+static int focus_mode = 1;
+
+EAPI int
+wizard_page_init(E_Wizard_Page *pg)
+{
+   return 1;
+}
+EAPI int
+wizard_page_shutdown(E_Wizard_Page *pg)
+{
+   return 1;
+}
+EAPI int
+wizard_page_show(E_Wizard_Page *pg)
+{
+   Evas_Object *o, *of, *ob;
+   E_Radio_Group *rg;
+   
+   o = e_widget_list_add(pg->evas, 1, 0);
+   e_wizard_title_set(_("Window Focus"));
+   
+   of = e_widget_framelist_add(pg->evas, _("Focus mode"), 0);
+
+   rg = e_widget_radio_group_new(&focus_mode);
+   
+   ob = e_widget_radio_add(pg->evas, _("Click to focus windows"), 0, rg);
+   e_widget_framelist_object_append(of, ob);
+   evas_object_show(ob);
+   ob = e_widget_radio_add(pg->evas, _("Mouse over focuses windows"), 1, rg);
+   e_widget_framelist_object_append(of, ob);
+   evas_object_show(ob);
+   
+   e_widget_list_object_append(o, of, 0, 0, 0.5);
+   evas_object_show(ob);
+   evas_object_show(of);
+
+   e_wizard_page_show(o);
+   pg->data = of;
+   return 1; /* 1 == show ui, and wait for user, 0 == just continue */
+}
+EAPI int
+wizard_page_hide(E_Wizard_Page *pg)
+{
+   evas_object_del(pg->data);
+   return 1;
+}
+EAPI int
+wizard_page_apply(E_Wizard_Page *pg)
+{
+   if (!focus_mode)
+     {
+	// FIXME: click to focus
+     }
+   else
+     {
+	// FIXME: sloppy focus
+     }
+   return 1;
+}
