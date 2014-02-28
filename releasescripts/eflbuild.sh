647c0d07efb063af4883d3a59ec340115be03427
Kristi Hoogland <kristi@kristi-book.(none)>
2014-02-28 11:59:10 -0600

    Updated control files for EFL and Elementary

--- releasescripts/eflbuild.sh
+++ releasescripts/eflbuild.sh
@@ -11,12 +11,12 @@ cp /media/sda5/Bodhi/e17_debs/controlfiles/efl/* debian/
 dpkg-buildpackage -rfakeroot -b
 dpkg -i ../efl*.deb
 
-cp -R /home/jeff/$2/efl-$2 /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1
-cd /media/sda5/Bodhi/e17_debs/$1
-tar czvf /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1.tar.gz efl-dbg_$1/
+#cp -R /home/jeff/$2/efl-$2 /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1
+#cd /media/sda5/Bodhi/e17_debs/$1
+#tar czvf /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1.tar.gz efl-dbg_$1/
 
-cd /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1
-dh_make -p efl-dbg_$1 -e jeffhoogland@linux.com -f ../efl-dbg_$1.tar.gz
-make distclean
-cp /media/sda5/Bodhi/e17_debs/controlfiles/efl-dbg/* debian/
-dpkg-buildpackage -rfakeroot -b
+#cd /media/sda5/Bodhi/e17_debs/$1/efl-dbg_$1
+#dh_make -p efl-dbg_$1 -e jeffhoogland@linux.com -f ../efl-dbg_$1.tar.gz
+#make distclean
+#cp /media/sda5/Bodhi/e17_debs/controlfiles/efl-dbg/* debian/
+#dpkg-buildpackage -rfakeroot -b
--- releasescripts/elementarybuild.sh
+++ releasescripts/elementarybuild.sh
@@ -11,12 +11,12 @@ cp /media/sda5/Bodhi/e17_debs/controlfiles/elementary/* debian/
 dpkg-buildpackage -rfakeroot -b
 dpkg -i ../elementary*.deb
 
-cp -R /home/jeff/$2/elementary-$2 /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1
-cd /media/sda5/Bodhi/e17_debs/$1
-tar czvf /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1.tar.gz elementary-dbg_$1/
+#cp -R /home/jeff/$2/elementary-$2 /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1
+#cd /media/sda5/Bodhi/e17_debs/$1
+#tar czvf /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1.tar.gz elementary-dbg_$1/
 
-cd /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1
-dh_make -p elementary-dbg_$1 -e jeffhoogland@linux.com -f ../elementary-dbg_$1.tar.gz
-make distclean
-cp /media/sda5/Bodhi/e17_debs/controlfiles/elementary-dbg/* debian/
-dpkg-buildpackage -rfakeroot -b
+#cd /media/sda5/Bodhi/e17_debs/$1/elementary-dbg_$1
+#dh_make -p elementary-dbg_$1 -e jeffhoogland@linux.com -f ../elementary-dbg_$1.tar.gz
+#make distclean
+#cp /media/sda5/Bodhi/e17_debs/controlfiles/elementary-dbg/* debian/
+#dpkg-buildpackage -rfakeroot -b
