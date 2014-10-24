6536083f056c18ccdbbc15e42709cc4004b5c8c5
Kai Huuhko <kai.huuhko@gmail.com>
2014-10-24 19:14:05 +0300

    Update e17 build files

--- 3.0.0/enlightenmentbuild.sh
+++ 3.0.0/enlightenmentbuild.sh
@@ -1,31 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-#cd /home/jeff/$2/enlightenment-$2 && make clean
-#./configure
-cp -R /home/jeff/$2/enlightenment-$2 /media/sda5/Bodhi/e17_debs/$1/enlightenment-$1
+EVERSION="0.17.6"
 
-#cd /media/sda5/Bodhi/e17_debs/$1/enlightenment-$1/src/modules
-#rm Makefile.in
-#rm Makefile_wizard.am
-#rm -rf wizard
-#cp -a /media/sda5/Bodhi/patches/e17/wizard/* .
+#rm -rf ~/enlightenment/
+rm enlightenment_${EVERSION}-1_i386.deb || echo -e "\e[31mOld .deb file not removed!\e[0m"
+#cp -R ~/e17/enlightenment/* ~/enlightenment
 
+echo
+cd ~/enlightenment
 
-cp /media/sda5/Bodhi/patches/e17/illume-keyboard/* /media/sda5/Bodhi/e17_debs/$1/enlightenment-$1/src/modules/illume-keyboard/keyboards/
+git clean -xdf || exit $?
+git checkout . || exit $?
+#patch -p1 < ~/buildscripts/bodhibuildscripts/patches/bodhi_e17.diff
+patch -p1 < ~/bodhibuildscripts/patches/bodhi_e17.diff || exit $?
 
-cd /media/sda5/Bodhi/e17_debs/$1/enlightenment-$1/src/bin
-#patch < /media/sda5/Bodhi/patches/e17/e/bodhi_menu.diff
-#patch < /media/sda5/Bodhi/patches/e17/e/module-patch.diff
-#patch -p0 < /media/sda5/Bodhi/patches/e/e_config.c.patch
+./autogen.sh --prefix=/usr || exit $?
+make dist || exit $?
+mv *.tar.* ..
+dh_make -y -m -e deepspeed@bodhilinux.com -f ../enlightenment-${EVERSION}.tar.gz -p enlightenment_${EVERSION} || exit $?
 
-cd /media/sda5/Bodhi/e17_debs/$1
-tar czvf enlightenment-$1.tar.gz enlightenment-$1/
+#mkdir debian/ && cp -R ../Documents/debian/* debian/
+cp -R ~/bodhibuildscripts/controlfiles/enlightenment/* debian/ || exit $?
+#nano -w debian/control
 
+dpkg-buildpackage -rfakeroot -b || exit $?
+#sudo dpkg -i ../enlightenment*.deb || exit $?
 
-cd /media/sda5/Bodhi/e17_debs/$1/enlightenment-$1
-make distclean
-dh_make -e jeffhoogland@linux.com -f ../enlightenment-$1.tar.gz
-
-cp -f /media/sda5/Bodhi/bodhibuildscripts/controlfiles/enlightenment/* debian/
-dpkg-buildpackage -rfakeroot -b
-dpkg -i ../enlightenment*.deb
+echo -e "\e[33mEnlightenment built succesfully!\e[0m"
+exit
