8556a6117d0f637dfd42a0f10770b45295e918f9
Jeff Hoogland <jeffhoogland@linux.com>
2015-01-14 11:39:13 -0600

    Updates to e17/e19 scripts

--- 3.0.0/e17build.sh
+++ 3.0.0/e17build.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 
-#cd ~/$2/e17-$2 && make clean
-#./configure
 cp -R ~/$2/e17-$2 /media/sda5/Bodhi/e17_debs/$1/e17-$1
-
-#cd /media/sda5/Bodhi/e17_debs/$1/e17-$1/src/modules
-#rm Makefile.in
-#rm Makefile_wizard.am
-#rm -rf wizard
-#cp -a /media/sda5/Bodhi/patches/e17/wizard/* .
-
 
 cp /media/sda5/Bodhi/patches/e17/illume-keyboard/* /media/sda5/Bodhi/e17_debs/$1/e17-$1/src/modules/illume-keyboard/keyboards/
 
-cd /media/sda5/Bodhi/e17_debs/$1/e17-$1/src/bin
-#patch < /media/sda5/Bodhi/patches/e17/e/bodhi_menu.diff
-#patch < /media/sda5/Bodhi/patches/e17/e/module-patch.diff
-#patch -p0 < /media/sda5/Bodhi/patches/e/e_config.c.patch
+cd /media/sda5/Bodhi/e17_debs/$1/e17-$1
+patch -p1 < /media/sda5/Bodhi/bodhibuildscripts/patches/bodhi_e17.diff
 
 cd /media/sda5/Bodhi/e17_debs/$1
 tar czvf e17-$1.tar.gz e17-$1/
-
 
 cd /media/sda5/Bodhi/e17_debs/$1/e17-$1
 make distclean
