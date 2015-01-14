65a1fc11c808b2c52761b858ab8ba1ddc142dc39
Jeff Hoogland <jeffhoogland@linux.com>
2015-01-14 14:58:41 -0600

    Updates to e17/19 scripts

--- 3.0.0/e17build.sh
+++ 3.0.0/e17build.sh
@@ -10,12 +10,12 @@ cp -R ../../$2/enlightenment-$2 ../../bodhi_debs/$1/e17-$1
 cd ../../bodhi_debs/$1/e17-$1
 patch -p1 < ../../../bodhibuildscripts/patches/bodhi_e17.diff
 
-cd ../../bodhi_debs/$1
+cd ..
 tar czvf e17-$1.tar.gz e17-$1/
 
-cd ../../bodhi_debs/$1/e17-$1
+cd e17-$1
 dh_make -e "Doug Yanez <deepspeed@bodhilinux.com>" -f ../e17-$1.tar.gz
 
-cp -f ../controlfiles/e17/* debian/
+cp -f ../../../bodhibuildscripts/controlfiles/e17/* debian/
 dpkg-buildpackage -rfakeroot -b
 sudo dpkg -i ../e17*.deb
--- 3.0.0/e19build.sh
+++ 3.0.0/e19build.sh
@@ -11,14 +11,12 @@ cp -R ../../$2/enlightenment-$2 ../../bodhi_debs/$1/e19-$1
 cd ../../bodhi_debs/$1/e19-$1
 patch -p1 < ../../../bodhibuildscripts/patches/bodhi_e19.diff
 
-./autogen.sh
-
-cd ../../bodhi_debs/$1
+cd ..
 tar czvf e19-$1.tar.gz e19-$1/
 
-cd ../../bodhi_debs/$1/e19-$1
+cd e19-$1
 dh_make -e "Eric W. Brown (Feneric)" -f ../e19-$1.tar.gz
 
-cp -f ../controlfiles/e19/* debian/
+cp -f ../../../bodhibuildscripts/controlfiles/e19/* debian/
 dpkg-buildpackage -rfakeroot -b
 sudo dpkg -i ../e19*.deb
