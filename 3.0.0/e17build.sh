0d946d1a52841bdbe38bbdc9d0690c26ce22a0f3
Jeff Hoogland <jeffhoogland@linux.com>
2015-01-14 23:52:46 -0600

    Added e17 patch back

--- 3.0.0/e17build.sh
+++ 3.0.0/e17build.sh
@@ -8,7 +8,7 @@ autoreconf
 cp -R ../../$2/enlightenment-$2 ../../bodhi_debs/$1/e17-$1
 
 cd ../../bodhi_debs/$1/e17-$1
-#patch -p1 < ../../../bodhibuildscripts/patches/bodhi_e17.diff
+patch -p1 < ../../../bodhibuildscripts/patches/bodhi_e17.diff
 
 cd ..
 tar czvf e17-$1.tar.gz e17-$1/
