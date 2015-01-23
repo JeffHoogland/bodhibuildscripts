60864bf2eeff7e7418d165078cdbb055dfa0ea7e
Jeff Hoogland <jeffhoogland@linux.com>
2015-01-23 09:47:40 -0600

    Added a genericbuild.sh for packing things that do not need extra care or patches

---
+++ 3.0.0/genericbuild.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p ../../bodhi_debs/$1
+
+cd ../../$2/$3-$2
+autoreconf
+
+cp -R ../../$2/$3-$2 ../../bodhi_debs/$1/$3-$1
+cd ../../bodhi_debs/$1
+tar czvf ../../bodhi_debs/$1/$3-$1.tar.gz $3-$1/
+
+cd ../../bodhi_debs/$1/$3-$1
+dh_make -e jeffhoogland@linux.com -f ../$3-$1.tar.gz
+make distclean
+cp ../controlfiles/$3/* debian/
+dpkg-buildpackage -rfakeroot -b
+dpkg -i ../$3*.deb
