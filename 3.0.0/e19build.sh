dbb3a06303d1d50e7d05e16e209d503cb5cf6c22
Jeff Hoogland <jeffhoogland@linux.com>
2015-08-24 09:47:22 -0500

    commented out bodhi patches from e19 build script

--- 3.0.0/e19build.sh
+++ 3.0.0/e19build.sh
@@ -15,15 +15,15 @@ else
     cp -R ../../$2/enlightenment-$2 ../../bodhi_debs/$1/e19-$1
 fi
 
-cd ../../bodhi_debs/$1/e19-$1
-rm -rf src/modules/wizard
-cp -a ../../../bodhibuildscripts/patches/wizard-e19 src/modules/wizard
+#cd ../../bodhi_debs/$1/e19-$1
+#rm -rf src/modules/wizard
+#cp -a ../../../bodhibuildscripts/patches/wizard-e19 src/modules/wizard
 
-rm src/modules/systray/e_mod_main.c
-cp ../../../bodhibuildscripts/patches/e_mod_main-e19-systray.c src/modules/systray/e_mod_main.c
+#rm src/modules/systray/e_mod_main.c
+#cp ../../../bodhibuildscripts/patches/e_mod_main-e19-systray.c src/modules/systray/e_mod_main.c
 
-rm -rf src/bin/e_int_menus.c
-cp ../../../bodhibuildscripts/patches/e_int_menus-e19.c src/bin/e_int_menus.c
+#rm -rf src/bin/e_int_menus.c
+#cp ../../../bodhibuildscripts/patches/e_int_menus-e19.c src/bin/e_int_menus.c
 
 #cd ../../bodhi_debs/$1/e19-$1
 #patch -p1 < ../../../bodhibuildscripts/patches/bodhi_e19.diff
@@ -36,4 +36,4 @@ dh_make -e "Jeff Hoogland" -f ../e19-$1.tar.gz
 
 cp -f ../../../bodhibuildscripts/controlfiles/e19/* debian/
 dpkg-buildpackage -rfakeroot -b
-sudo dpkg -i ../e19*.deb
+#sudo dpkg -i ../e19*.deb
