83dbc1e4a8bad02eaad8ac3af45bb55d1b19e6ff
hippytaff <hippytaff@gmail.com>
2016-01-18 17:04:03 +0000

    added todo

--- getdeps.sh
+++ getdeps.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Script to get dependencies for building deb fils
 
-# get list of libs for application
-# if package is installed check /usr/bin/
+# get list of libs for application and find package names for deps
+#
+# Todo
+# objdump for bins - http://www.askrprojects.net/software/objdump.html
+# check ./configure first
+# add i686 for list parse
 
+# if package is installed check /usr/bin/
 PKG="$(which $1)"
 objdump -p /usr/bin/$1 | grep NEEDED | sed -e 's/NEEDED//g' > tmp
 LIB="$(cat tmp)"
