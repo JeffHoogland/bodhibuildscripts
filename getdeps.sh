6978946b51dc636ed2a0cd1477661e16906d091d
hippytaff <hippytaff@gmailcom>
2016-01-18 12:34:44 +0000

    working roughly

--- getdeps.sh
+++ getdeps.sh
@@ -2,12 +2,13 @@
 # Script to get dependencies for building deb fils
 
 # get list of libs for application
+# if package is installed check /usr/bin/
 
 PKG=$1
 objdump -p /usr/bin/$1 | grep NEEDED | sed -e 's/NEEDED//g' > tmp
 LIB="$(cat tmp)"
 dpkg -S $LIB > list
 rm tmp
-awk '{ print $1 }' list | sort | uniq
-
+awk '{ print $1 }' list | sort | uniq | sed -e 's/i386://g'
+rm list
 
